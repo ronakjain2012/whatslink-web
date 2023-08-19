fd7204365d669a76dd2684c3619edb5984af46a5
Ronak Bokaria <ronakjain2012@gmail.com>
2023-08-19 14:57:33 +0530

    UI

--- src/widgets/layout/footer.jsx
+++ src/widgets/layout/footer.jsx
@@ -10,7 +10,7 @@ export function Footer({ brandName, brandLink, routes }) {
       <div className="flex w-full flex-wrap items-center justify-center gap-6 px-2 md:justify-between">
         <Typography variant="small" className="font-normal text-inherit">
           &copy; {year}, made with{" "}
-          <HeartIcon className="-mt-0.5 inline-block h-3.5 w-3.5" /> by{" "}
+          <HeartIcon className="-mt-0.5 inline-block h-3.5 w-3.5 text-danger-500" /> by{" "}
           <a
             href={brandLink}
             target="_blank"
@@ -41,13 +41,13 @@ export function Footer({ brandName, brandLink, routes }) {
 }
 
 Footer.defaultProps = {
-  brandName: "Creative Tim",
-  brandLink: "https://www.creative-tim.com",
+  brandName: "Ronak Bokaria (@ronakjain2012) ",
+  brandLink: "https://bit.ly/ronakjain2012",
   routes: [
-    { name: "Creative Tim", path: "https://www.creative-tim.com" },
-    { name: "About Us", path: "https://www.creative-tim.com/presentation" },
-    { name: "Blog", path: "https://www.creative-tim.com/blog" },
-    { name: "License", path: "https://www.creative-tim.com/license" },
+    // { name: "Creative Tim", path: "https://www.creative-tim.com" },
+    // { name: "About Us", path: "https://www.creative-tim.com/presentation" },
+    // { name: "Blog", path: "https://www.creative-tim.com/blog" },
+    // { name: "License", path: "https://www.creative-tim.com/license" },
   ],
 };
 
